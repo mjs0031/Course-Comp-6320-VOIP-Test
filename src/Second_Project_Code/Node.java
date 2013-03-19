fefded51e0a7873e9fe500656515756bdd0b500e
Ian Middleton <ian.r.middleton@gmail.com>
2013-03-18 21:46:21 -0500

    Finished Node Class

--- src/Second_Project_Code/Node.java
+++ src/Second_Project_Code/Node.java
@@ -1,8 +1,6 @@
 // Package Declaration //
 package Second_Project_Code;
 
-import java.io.IOException;
-
 // Java Package Support //
 // { Not Applicable }
 
@@ -33,6 +31,7 @@ public class Node{
 	int number, port, x, y;
 	string address;
 	Node[] links;
+	int numLinks;
 	
 	/**
      * Constructor for the Node class.
@@ -44,15 +43,131 @@ public class Node{
 	 * @param  y           	: Y coordinate of the node.
 	 * @param  links        : An array that contains all nodes that this node
 	 * 							is linked to.
+	 * @param numLinks		: The number of links in the array.
      */
-	public Node(int number, string address, int port, int x, int y, Node[] links){
+	public Node(int number, string address, int port, int x, int y){
 		this.number = number;
 		this.address = address;
 		this.port = port;
 		this.x = x;
 		this.y = y;
 		this.links = links;
+		this.numLinks = numLinks;
 	} // end Node()
+	
+	/**
+	 * Gets the number of the node.
+	 * 
+	 * @return 				: An int representing the node.
+	 */
+	public int getNumber(){
+		return number;
+	} // end getNumber()
+	
+	/**
+	 * Gets the IP address of the node.
+	 * 
+	 * @return				: A string containing the IP address.
+	 */
+	public string getAddress(){
+		return address;
+	} // end getAddress()
+	
+	/**
+	 * Gets the port of the node.
+	 * 
+	 * @return				: An int containing the port number.
+	 */
+	public int getPort(){
+		return port;
+	} // end getPort()
+	
+	/**
+	 * Gets the X coordinate of the node.
+	 * 
+	 * @return				: An int containing the X coordinate.
+	 */
+	public int getX(){
+		return x;
+	} // end getX()
+	
+	/**
+	 * Sets the X coordinate of the node.
+	 * 
+	 * @param x				: What the X coordinate is to be changed to.
+	 */
+	public void setX(int x){
+		this.x = x;
+	} // end setX()
+	
+	/**
+	 * Gets the Y coordinate of the node.
+	 * 
+	 * @return				: An int containing the Y coordinate.
+	 */
+	public int getY(){
+		return y;
+	}
+	
+	/**
+	 * Sets the Y coordinate of the node.
+	 * 
+	 * @param y				: What the Y coordinate is to be changed to.
+	 */
+	public void setY(int y){
+		this.y = y;
+	} // end setY()
+	
+	/**
+	 * Gets an array containing all the nodes this node is linked to.
+	 * 
+	 * @return				: A node array containing all nodes this node
+	 * 							is linked to.
+	 */
+	public Node[] getLinks(){
+		return links;
+	} // end getLinks()
+	
+	/**
+	 * Adds a new node to the list of nodes this node is linked to.
+	 * 
+	 * @param newNode		: The node to be added to the list of nodes which
+	 * 							this node is linked to.
+	 */
+	public void addLink(Node newNode){
+		links[numLinks] = newNode;
+		numLinks++;
+	} // end addLink
+	
+	/**
+	 * Removes a node from the list of nodes this node is linked to.
+	 * 
+	 * @param nodeNumber	: The number associated with the node which will be
+	 * 							removed from the list of nodes linked with this one.
+	 */
+	public void removeLink(int nodeNumber){
+		notFound = true;
+		currentSlot = 0;
+		for(int i = 0; i < numLinks; i++){
+			if(notFound){
+				if(links[currentSlot].getNumber == nodeNumber){
+					notFound = false;
+				}
+			}else{
+				links[currentSlot-1] = links[currentSlot];
+			}
+		}
+		numLinks--;
+	} // end removeLink()	
+	
+	/**
+	 * Removes all the nodes currently linked to this node from the links array and
+	 * resets the number of links back to 0.
+	 */
+	public void removeAllLinks(){
+		links = new Node[];
+		numLinks = 0;
+	} // end removeAllLinks()
 	
 } // end Node class
 
